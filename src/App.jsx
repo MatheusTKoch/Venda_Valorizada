bc82f502e7f23d657d20d38297a94d3283c2a8b3
Matheus <matheustrilhakoch@hotmail.com>
2024-03-25 21:20:04 -0300

    Adding buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
 import './App.css'
 import Barra from './Barra.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Barra></Barra>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          Login
+        </button>
+        <p></p>
+        <button>
+          Criar Conta
         </button>
       </div>
     </>
