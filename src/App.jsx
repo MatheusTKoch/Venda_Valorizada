57ebb0123f1e332cacd4d713c40ad98af27e78f9
Matheus <matheustrilhakoch@hotmail.com>
2024-03-31 22:22:28 -0300

    Testing register

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,57 @@
 import './App.css'
 import Barra from './Barra.jsx'
+import { useState } from 'react';
 
 function App() {
+    const [formData, setFormData] = useState({
+      username: '',
+      email: '',
+      password: ''
+    });
+  
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleRegister = async e => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5173/api/users/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
       <Barra></Barra>
       <div className="card">
+        <form onSubmit={handleRegister}>
         <div>
           <label htmlFor="email">Email: </label>
-          <input type="text" id="user"></input>
+          <input type="text" id="email" value={formData.email} onChange={handleChange}></input>
           <p></p>
-          <label htmlFor="user">Senha: </label>
-          <input type="password" id="user"></input>
+          <label htmlFor="password">Senha: </label>
+          <input type="password" id="password" value={formData.password} onChange={handleChange}></input>
         </div>
         <p></p>
         <div>
           <button>
             Login
           </button>
-          <button>
+          <button type="submit">
             Criar Conta
           </button>
         </div>
+        </form>
       </div>
     </>
   )
