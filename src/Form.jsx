27a4a5aa4b0ed56a559718d5fe838dfc15f3fee3
Matheus <matheustrilhakoch@hotmail.com>
2024-05-27 20:22:21 -0300

    Movendo funcoes de logica para Form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,6 +1,51 @@
 import './App.css'
+import { useState } from 'react';
 
-function app() {
+function App() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+      });
+    
+    const handleChange = e => {
+      setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+  
+  
+    const handleRegister = async e => {
+      e.preventDefault();
+      try {
+        const response = await fetch('http://localhost:5173/api/users/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        });
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    const handleLogin = async e => {
+      e.preventDefault();
+      try {
+        const response = await fetch('http://localhost:5173/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        });
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     return (
         <>
         <hgroup>
@@ -8,8 +53,28 @@ function app() {
         <h2>O aplicativo para facilitar seus calculos!</h2>
         </hgroup>
         <p>Como utilizar: apos login, cadastre o produto, seus custos e ao final adicione a margem de lucro!</p>
+        <div className="card">
+        <form>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input type="text" id="email" name="email" value={formData.email} onChange={handleChange}></input>
+          <p></p>
+          <label htmlFor="password">Senha: </label>
+          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange}></input>
+        </div>
+        <p></p>
+        <div>
+          <button type="submit" onClick={handleLogin}>
+            Login
+          </button>
+          <button type="submit" onClick={handleRegister}>
+            Criar Conta
+          </button>
+        </div>
+        </form>
+      </div>
         </>
     )
 }
 
-export default app
+export default App
