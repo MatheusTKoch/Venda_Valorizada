4f6cd975f9d4d421f399249e5e012eff6853b50a
Matheus <matheustrilhakoch@hotmail.com>
2024-05-27 20:17:21 -0300

    Alterando img

--- src/Barra.jsx
+++ src/Barra.jsx
@@ -3,7 +3,12 @@ import './App.css';
 function app() {
     return (
         <>
-        <div className='rodapeSuperior'></div>
+        <div className='rodapeSuperior'>
+            <div className="row">
+                <img alt="logo" src="/src/assets/logo.png">
+                </img>
+            </div>
+        </div>
         </>
     )
 }
--- src/Form.jsx
+++ src/Form.jsx
@@ -3,10 +3,6 @@ import './App.css'
 function app() {
     return (
         <>
-        <div className="row">
-            <img alt="logo" src="/src/assets/logo.png">
-            </img>
-        </div>
         <hgroup>
         <h1>Venda Valorizada</h1>
         <h2>O aplicativo para facilitar seus calculos!</h2>
