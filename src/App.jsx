86c771c77d02eb1c6fdde02d8c8640413017b734
Matheus <matheustrilhakoch@hotmail.com>
2024-06-09 00:07:22 -0300

    Ajustando front

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import Form from './Form.jsx';
 import TopHeader from './TopHeader.jsx';
-
+import Dashboard from './Dashboard.jsx';
 
 function App() {
 
   return (
     <>
     <Router>
-      <TopHeader></TopHeader>
-      <Form></Form>
+      <div>
+        <TopHeader></TopHeader>
+        <Switch>
+          <Route path="/login" component={Form} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Redirect from="/" to="/login" />
+        </Switch> 
+      </div>  
     </Router>
     </>
   )
