da54e69713ce75a3ae88a81df2a4791d5eab4257
Matheus <matheustrilhakoch@hotmail.com>
2024-06-16 17:14:28 -0300

    Adicionando arquivo de configuracao sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql'
+});
+
+export default sequelize;
+
