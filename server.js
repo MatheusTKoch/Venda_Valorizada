f4a94affc4c952d7b0ccd1c4701b18ff4e11a3ad
Matheus <matheustrilhakoch@hotmail.com>
2024-06-16 18:09:16 -0300

    Ajustando serverJS

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./src/routes/user.js');
 
@@ -14,7 +13,15 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
-mongoose.connect(process.env.DATABASE_URL);
+sequelize.sync()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
