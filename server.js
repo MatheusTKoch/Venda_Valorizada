6ef9f133906d6239b33d3fa83f1943782779a109
Matheus <matheustrilhakoch@hotmail.com>
2024-06-27 21:13:34 -0300

    Iniciando testes na port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const sequelize = require('./src/config/database.js');
 const userRoutes = require('./src/routes/user.js');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
