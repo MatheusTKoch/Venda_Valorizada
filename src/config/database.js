67d1d98c5849f7a5a2e4aee69872818ffca11494
Matheus <matheustrilhakoch@hotmail.com>
2024-07-07 10:22:32 -0300

    Fazendo testes na conexao, ainda nao finalizado

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-undef */
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(import.meta.MARIADB_DATABASE, import.meta.MARIADB_USER, import.meta.MARIADB_PASSWORD, {
-    host: import.meta.MARIADB_HOST,
-    dialect: 'mariadb'
+const dbName = String(import.meta.MARIADB_DATABASE);
+const dbUser = String(import.meta.MARIADB_USER);
+const dbPassword = String(import.meta.MARIADB_PASSWORD);
+const dbHost = String(import.meta.MARIADB_HOST);
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    dialect: 'mariadb',
+    port: 3307
 });
 
 export default sequelize;
